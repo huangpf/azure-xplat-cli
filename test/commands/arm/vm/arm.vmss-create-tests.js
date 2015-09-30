e858f21f6fe3de5c5ae1557d8e77620e968f39db
huangpf <phuang@microsoft.com>
2015-09-29 22:58:24 -0700

    test

---
+++ test/commands/arm/vm/arm.vmss-create-tests.js
@@ -0,0 +1,174 @@
+/**
+ * Copyright (c) Microsoft.  All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+var should = require('should');
+var path = require('path');
+var fs = require('fs');
+var util = require('util');
+var testUtils = require('../../../util/util');
+var CLITest = require('../../../framework/arm-cli-test');
+var testprefix = 'arm-cli-vmss-create-tests';
+var groupPrefix = 'xplatTestVMSSCreate';
+var VMTestUtil = require('../../../util/vmTestUtil');
+var requiredEnvironment = [{
+  name: 'AZURE_VM_TEST_LOCATION',
+  defaultValue: 'southeastasia'
+}, {
+  name: 'SSHCERT',
+  defaultValue: 'test/myCert.pem'
+}];
+
+var groupName,
+  vmPrefix = 'xplattestvm',
+  nicName = 'xplattestnic',
+  location,
+  username = 'azureuser',
+  password = 'Brillio@2015',
+  storageAccount = 'xplatteststorage1',
+  storageCont = 'xplatteststoragecnt1',
+  osdiskvhd = 'xplattestvhd',
+  vNetPrefix = 'xplattestvnet',
+  subnetName = 'xplattestsubnet',
+  publicipName = 'xplattestip',
+  dnsPrefix = 'xplattestipdns',
+  tags = 'a=b;b=c;d=',
+  sshcert,
+  IaasDiagPublisher,
+  IaasDiagExtName,
+  IaasDiagVersion,
+  datafile = 'test/data/testdata.json',
+  paramFileName = './vmssParamTest.json'
+
+describe('arm', function() {
+  describe('compute', function() {
+    var suite, retry = 5;
+    var vmTest = new VMTestUtil();
+    before(function(done) {
+      suite = new CLITest(this, testprefix, requiredEnvironment);
+      suite.setupSuite(function() {
+        location = process.env.AZURE_VM_TEST_LOCATION;
+        sshcert = process.env.SSHCERT;
+        groupName = suite.generateId(groupPrefix, null);
+        vmPrefix = suite.isMocked ? vmPrefix : suite.generateId(vmPrefix, null);
+        nicName = suite.isMocked ? nicName : suite.generateId(nicName, null);
+        storageAccount = suite.generateId(storageAccount, null);
+        storageCont = suite.generateId(storageCont, null);
+        osdiskvhd = suite.isMocked ? osdiskvhd : suite.generateId(osdiskvhd, null);
+        vNetPrefix = suite.isMocked ? vNetPrefix : suite.generateId(vNetPrefix, null);
+        subnetName = suite.isMocked ? subnetName : suite.generateId(subnetName, null);
+        publicipName = suite.isMocked ? publicipName : suite.generateId(publicipName, null);
+        dnsPrefix = suite.generateId(dnsPrefix, null);
+        tags = 'a=b;b=c;d=';
+
+        // Get real values from test/data/testdata.json file and assign to the local variables
+        var data = fs.readFileSync(datafile, 'utf8');
+        var variables = JSON.parse(data);
+        IaasDiagPublisher = variables.IaasDiagPublisher_Linux.value;
+        IaasDiagExtName = variables.IaasDiagExtName_Linux.value;
+        IaasDiagVersion = variables.IaasDiagVersion_Linux.value;
+        done();
+      });
+    });
+    after(function(done) {
+      vmTest.deleteUsedGroup(groupName, suite, function(result) {
+        suite.teardownSuite(done);
+      });
+    });
+    beforeEach(function(done) {
+      suite.setupTest(done);
+    });
+    afterEach(function(done) {
+      suite.teardownTest(done);
+    });
+
+    describe('vmss', function() {
+
+      it('group create should pass', function(done) {
+        this.timeout(vmTest.timeoutLarge);
+        vmTest.checkImagefile(function() {
+          vmTest.createGroup(groupName, location, suite, function(result) {
+            var cmd = util.format('location list').split(' ');
+            testUtils.executeCommand(suite, retry, cmd, function(result) {
+              result.exitStatus.should.equal(0);
+            });
+            /* TODO: The following commented tests won't pass in Playback mode. */
+
+            /*// Create the parameter file
+            var cmd = util.format('vmss parameters generate create-or-update --parameter-file ' + paramFileName).split(' ');
+            testUtils.executeCommand(suite, retry, cmd, function(result) {
+              result.exitStatus.should.equal(0);
+            });
+
+            // Patch replace the parameter file
+            var cmd = util.format('vmss parameters patch --parameter-file ' + paramFileName + ' --operation replace --path /tags --value {\"key\":\"test4\"}').split(' ');
+            testUtils.executeCommand(suite, retry, cmd, function(result) {
+              result.exitStatus.should.equal(0);
+              done();
+            });*/
+          });
+        });
+      });
+
+      /* TODO: The following commented tests won't run. */
+
+      /*it('parameters patch add should pass', function(done) {
+        this.timeout(vmTest.timeoutLarge);
+        // Patch replace the parameter file
+        var cmd = util.format('vmss parameters patch --parameter-file ' + paramFileName + ' --operation replace --path /tags --value {\"key\":\"test4\"}').split(' ');
+        testUtils.executeCommand(suite, retry, cmd, function(result) {
+          result.exitStatus.should.equal(0);
+        });
+        // Patch the parameter file
+        var cmd = util.format('vmss parameters patch --parameter-file ' + paramFileName + ' --operation add --path /test1 --value "test2"').split(' ');
+        testUtils.executeCommand(suite, retry, cmd, function(result) {
+          result.exitStatus.should.equal(0);
+          done();
+        });
+      });
+
+      it('parameters patch add should pass again', function(done) {
+        this.timeout(vmTest.timeoutLarge);
+        // Patch add the parameter file
+        var cmd = util.format('vmss parameters patch --parameter-file ' + paramFileName + ' --operation add --path /test2 --value "test3"').split(' ');
+        testUtils.executeCommand(suite, retry, cmd, function(result) {
+          result.exitStatus.should.equal(0);
+          done();
+        });
+      });
+
+      it('parameters patch remove should pass', function(done) {
+        this.timeout(vmTest.timeoutLarge);
+        // Patch remove the parameter file
+        var cmd = util.format('vmss parameters patch --parameter-file ' + paramFileName + ' --operation remove --path /test1').split(' ');
+        testUtils.executeCommand(suite, retry, cmd, function(result) {
+          result.exitStatus.should.equal(0);
+          done();
+        });
+      });
+
+      it('parameters patch replace should pass', function(done) {
+        this.timeout(vmTest.timeoutLarge);
+        // Patch replace the parameter file
+        var cmd = util.format('vmss parameters patch --parameter-file ' + paramFileName + ' --operation replace --path /tags --value {\"key\":\"test4\"}').split(' ');
+        testUtils.executeCommand(suite, retry, cmd, function(result) {
+          result.exitStatus.should.equal(0);
+          done();
+        });
+      });*/
+
+    });
+  });
+});
